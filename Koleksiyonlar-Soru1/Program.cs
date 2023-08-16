d1b7d4f674e617618c75a68e1266c284d818f62d
Sercan <56640809+sercantrhnn@users.noreply.github.com>
2023-08-16 11:39:27 +0300

    Add files via upload

---
+++ Koleksiyonlar-Soru1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList sayiListesi = new ArrayList();
+            ArrayList asalList = new ArrayList();
+            ArrayList asalDegil = new ArrayList();
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.Write("Dizinin {0}. elemanını giriniz: ",i);
+                sayiListesi.Add(Console.ReadLine());
+            }
+
+            foreach (var item in sayiListesi)
+            {
+                if (Convert.ToInt32(item) ==2 || Convert.ToInt32(item) % 2 !=0)
+                {
+                    asalList.Add(item);
+                }
+                else
+                {
+                    asalDegil.Add(item);
+                }
+            }
+
+           asalList.Sort();
+           asalList.Reverse();
+           asalDegil.Sort();
+           asalDegil.Reverse();
+
+           foreach (var item in asalList)
+           {
+            Console.WriteLine(item);
+           }
+
+           foreach (var item in asalDegil)
+           {
+            Console.WriteLine(item);
+           }
+
+        
+           
+
+            
+        }
+    }
+}
---
+++ Koleksiyonlar-Soru2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Koleksiyonlar_Soru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[7];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write("Dizinin {0}. elemanını giriniz.", i+1);
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(sayilar);
+            int[] sayilarKucuk = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                sayilarKucuk[i] = sayilar[i];
+            }
+
+            int[] sayilarBuyuk = new int[3];
+            Array.Reverse(sayilar);
+            for (int i = 0; i < 3; i++)
+            {
+                sayilarBuyuk[i] = sayilar[i];
+            }
+            int toplamBuyuk = 0;
+            foreach (var item in sayilarBuyuk)
+            {
+                toplamBuyuk += item;
+            }
+            Console.Write("Büyük sayıların ortalaması: ");
+            Console.WriteLine(toplamBuyuk/sayilarBuyuk.Length);
+
+            int toplamKucuk = 0;
+            foreach (var item in sayilarKucuk)
+            {
+                toplamKucuk += item;
+            }
+
+            Console.Write("Küçük sayıların ortalaması: ");
+            Console.WriteLine(toplamKucuk/sayilarKucuk.Length);
+        }
+    }
+}
